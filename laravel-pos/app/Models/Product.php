7bf6a578ea861e0f92319545080a1c5f6ba88006
Man4hard <107409042+Man4hard@users.noreply.github.com>
2026-04-03 19:33:47 +0500

    Add laravel-pos/app/Models/Product.php

---
+++ laravel-pos/app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    protected $fillable = ['name', 'description', 'price', 'category_id', 'image_url', 'available'];
+
+    protected $casts = [
+        'price' => 'float',
+        'available' => 'boolean',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
