01a2be2de856daca0ff019d90616458a2ef71f68
Man4hard <107409042+Man4hard@users.noreply.github.com>
2026-04-06 15:03:54 +0500

    Add LocaleProvider with Noori Nastaleeq font support

---
+++ flutter-pos/lib/services/locale_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../theme/app_theme.dart';
+import '../l10n/app_strings.dart';
+
+class LocaleProvider extends ChangeNotifier {
+  bool _isUrdu = false;
+
+  bool get isUrdu => _isUrdu;
+  AppStrings get strings => AppStrings(isUrdu: _isUrdu);
+  TextDirection get textDirection =>
+      _isUrdu ? TextDirection.rtl : TextDirection.ltr;
+
+  ThemeData get theme {
+    if (_isUrdu) {
+      final urduTextTheme =
+          GoogleFonts.notoNastaliqUrduTextTheme(AppTheme.dark.textTheme);
+      return AppTheme.dark.copyWith(textTheme: urduTextTheme);
+    }
+    return AppTheme.dark;
+  }
+
+  void toggle() {
+    _isUrdu = !_isUrdu;
+    notifyListeners();
+  }
+}
